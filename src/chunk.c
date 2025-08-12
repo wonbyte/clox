39cf50a189a6cd8375964275f13502d1405979ad
Greg Sieranski <git@wonbyte.com>
2025-08-11 23:49:52 -0400

    continue work on value arrays

--- src/chunk.c
+++ src/chunk.c
@@ -2,11 +2,13 @@
 
 #include "chunk.h"
 #include "memory.h"
+#include "value.h"
 
 void initChunk(Chunk* chunk) {
   chunk->count = 0;
   chunk->capacity = 0;
   chunk->code = NULL;
+  initValueArray(&chunk->constants);
 }
 
 void writeChunk(Chunk* chunk, uint8_t byte) {
@@ -21,7 +23,13 @@ void writeChunk(Chunk* chunk, uint8_t byte) {
   chunk->count++;
 }
 
+int addConstant(Chunk* chunk, Value value) {
+  writeValueArray(&chunk->constants, value);
+  return chunk->constants.count - 1;
+}
+
 void freeChunk(Chunk* chunk) {
   FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+  freeValueArray(&chunk->constants);
   initChunk(chunk);
 }
